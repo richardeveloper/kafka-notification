6dcb70280ffb7bc75fef3ef59b1ff310ceddd1f4
richardeveloper <richard_ar@hotmail.com>
2024-09-24 22:05:30 -0300

    Initial commit

---
+++ src/main/java/br/com/kafka/principal/KafkaNotificationApplication.java
@@ -0,0 +1,13 @@
+package br.com.kafka.principal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaNotificationApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(KafkaNotificationApplication.class, args);
+  }
+
+}
---
+++ src/test/java/br/com/kafka/principal/KafkaNotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.kafka.principal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaNotificationApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
